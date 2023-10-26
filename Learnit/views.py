bc0374007084691c6bdbd5b65a142d6a5461dd3a
unknown <ojekab2580@gmail.com>
2023-10-26 15:06:40 -0700

    the

--- Learnit/views.py
+++ Learnit/views.py
@@ -6,9 +6,7 @@ from student.models import Student
 # Create your views here.
 def Index(request):
     user = request.user
-    student = Student.objects.get(user=user)
-    fetch_review = myReview.objects.all()
-    context = {'Review': fetch_review, "student": student, "user":user}
+    
     if request.method == 'POST':
         form = request.POST
         Name = form.get('Name')
@@ -24,9 +22,14 @@ def Index(request):
         review = myReview.objects.create(Name=Name, Email=Email, Nationality = Nationality, Body = Body)
         review.save()
         messages.success(request, 'Thanks for reviewing us')
+    
+    if not request.user.is_authenticated:
+        student = Student.objects.get(user=user)
+        fetch_review = myReview.objects.all()
+        context = {'Review': fetch_review, "student": student, "user":user}
         # return redirect(reverse(request, 'Learnit_Index_page'))
-    return render(request, 'Learnit/index.html', context)
-    # return render(request, 'Learnit/index.html')
+        return render(request, 'Learnit/index.html', context)
+    return render(request, 'Learnit/index.html')
 
 def Courses(request):
     return render(request, 'Learnit/courses.html')
