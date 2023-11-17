55e2918ce730d9b17b27fa9cebf2896d79118ba4
unknown <ojekab2580@gmail.com>
2023-11-16 20:11:18 -0800

    latest commit

--- Learnit/models.py
+++ Learnit/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from student.models import Student
 
 # Create your models here.
 
 class myReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank=True)
+    student = models.ForeignKey(Student,on_delete=models.PROTECT, null=True,blank=True)
     Name = models.CharField(max_length=40)
     Email = models.CharField(max_length=40)
     Nationality = models.CharField(max_length=15)
     Body = models.CharField(max_length=10000)
+    
+    def __str__(self):
+        return f"{self.id}--Review created by {self.student}"
+    
    
--- Learnit/views.py
+++ Learnit/views.py
@@ -5,30 +5,36 @@ from student.models import Student
 
 # Create your views here.
 def Index(request):
-    user = request.user
     
     if request.method == 'POST':
         form = request.POST
         Name = form.get('Name')
         Email = form.get('Email')
         Nationality = form.get('Nationality')
-        Body = form.get('Body')
-        
-        review = myReview.objects.create(Name=Name, Email=Email, Nationality = Nationality, Body = Body)
+        Body = form.get('Body')       
+        if request.user.is_authenticated:
+            student = request.user.student
+        else:
+            student = None
+            
+        review = myReview.objects.create(Name=Name, Email=Email, Nationality = Nationality, Body = Body, student=student)
         review.save()
         messages.success(request, 'Thanks for reviewing us')
         # return render(request, 'Learnit/index.html')
+    user = request.user
     
     fetch_review = myReview.objects.all()
-    context = {'Review': fetch_review, "user":user}
+    # for Review in fetch_review:
+    #     context = {'Review':Review}
+    context = {'Reviews': fetch_review, "user":user}
     if request.user.is_authenticated:
         student_exit = Student.objects.filter(user=user).exists()
         if student_exit:
             student = Student.objects.get(user=user)
             fetch_review = myReview.objects.all()
-            context = {'Review': fetch_review, "student": student, "user":user}
+            context = {'Reviews': fetch_review, "student": student, "user":user}
         else:
-            context = {'Review': fetch_review, "user":user}
+            context = {'Reviews': fetch_review, "user":user}
         return render(request, 'Learnit/index.html', context) 
     # context = {'Review': fetch_review, "student": student, "user":user}
     return render(request, 'Learnit/index.html', context)
